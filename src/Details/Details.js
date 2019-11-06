1678d25f0a0313bb1110ba421dfcdda49d2209b4
maxgdev <max@apphilia.net>
2019-11-06 21:12:07 +0000

    Completed home and details page routing with link back to '/' from '/details'

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Details.css';
 export default function Details() {
     return (
@@ -8,6 +9,7 @@ export default function Details() {
           <p className='item'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate rhoncus facilisis. Aliquam in mauris ut augue mattis pretium. Aenean vel elit urna. Fusce sit amet metus volutpat, efficitur lectus a, vestibulum nisi. Duis mollis nibh id venenatis blandit. Cras pharetra purus in arcu scelerisque, a pulvinar odio ullamcorper. Maecenas at lectus pulvinar, cursus purus ac, aliquet diam. In at venenatis dolor. Sed tincidunt imperdiet risus non semper.</p>
           <p className='item'>Quisque diam odio, cursus et ante a, varius commodo libero. Fusce eget metus urna. Vestibulum vulputate finibus tortor, sed vestibulum quam volutpat eget. Praesent consequat sapien ac porta fermentum. Proin vestibulum augue sit amet auctor scelerisque. Etiam euismod dignissim mauris ac commodo</p>
         </div>
+        <Link to='/'>Back to homepage</Link>
       </div>
     )
   }
--- src/TvShow/TvShow.js
+++ src/TvShow/TvShow.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './TvShow.css';
 export default function TvShow(props) {
-    return <div className='flexItem'>
+    return <Link to='/details' className='flexItem'>
       <img src={props.movieImage} alt={props.movieName + ' movie'} />
       <div className='overlay'>{props.movieName}</div>
-    </div>
+    </Link>
   }
