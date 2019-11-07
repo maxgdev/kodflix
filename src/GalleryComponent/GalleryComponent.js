f83f6fa0949cb4dd5e383dbeb42cd6f8409f9b08
maxgdev <max@apphilia.net>
2019-11-07 10:58:12 +0000

    Changed from flex to grid css layout #11

--- src/GalleryComponent/GalleryComponent.js
+++ src/GalleryComponent/GalleryComponent.js
@@ -8,19 +8,17 @@ import ironMan4Movie from '../images/iron-man-4.jpg';
 import avengersEndgameMovie from '../images/avengers-endgame.png';
 import blackPantherMovie from '../images/black-panther.jpg';
 export default function GalleryComponent() {
-    return (
-      <div>
-        <h1>Kodiri Kodflix App v0.01</h1>
-        <div className="flexContianer">
-          <TvShow id='matrix' movieImage={matrixMovie} movieName='Matrix' />
-          <TvShow  id='fullMetal' movieImage={fullMetalMovie} movieName='Full Metal Alchemist' />
-          <TvShow  id='bleach' movieImage={bleachMovie} movieName='Bleach' />
-        </div>
-        <div className="flexContianer">
-          <TvShow movieImage={ironMan4Movie} movieName='Iron Man 4' />
-          <TvShow movieImage={avengersEndgameMovie} movieName='Avengers Endgame' />
-          <TvShow movieImage={blackPantherMovie} movieName='Black Panther' />
-        </div>
+  return (
+    <div>
+      <h1>Kodiri Kodflix App v0.01</h1>
+      <div className="container">
+        <TvShow id='matrix' movieImage={matrixMovie} movieName='Matrix' />
+        <TvShow id='fullMetalAllchemist' movieImage={fullMetalMovie} movieName='Full Metal Alchemist' />
+        <TvShow id='bleach' movieImage={bleachMovie} movieName='Bleach' />
+        <TvShow id='ironMan4' movieImage={ironMan4Movie} movieName='Iron Man 4' />
+        <TvShow id='avengersEndgame' movieImage={avengersEndgameMovie} movieName='Avengers Endgame' />
+        <TvShow id='blackPanther' movieImage={blackPantherMovie} movieName='Black Panther' />
       </div>
-    )
-  }
+    </div>
+  )
+}
