aae9a54044c206060630f12ef557d321ae609440
maxgdev <max@apphilia.net>
2019-12-10 23:28:39 +0000

    Completed - Return a list of shows from the backend - Task #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const getmovies = require('./getmovies');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/rest/shows', (req, res) => res.send(getmovies()))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/backend/getmovies.js
@@ -0,0 +1,20 @@
+function getMovies() {
+    return [
+      {title: 'The Matrix',  id:'matrix',synopsis: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers'},
+      {title: 'Full Metal Alchemist',  id:'fullMetal',synopsis: 'When a failed alchemical ritual leaves brothers Edward and Alphonse Elric with severely damaged bodies, they begin searching for the one thing that can save them; the fabled philosopher\'s stone.'},
+      {title: 'Bleach',  id:'bleach',synopsis: 'High school student Kurosaki Ichigo is unlike any ordinary kid because he can see ghosts. After an accident with a hollow, he get powers. So begins Kurosaki Ichigo\'s training and duty as a Shinigami, Soul Reaper'},
+      {title: 'Iron Man',  id:'ironMan',synopsis: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.'},
+      {title: 'Avengers Endgame',  id:'avengersEndgame',synopsis: 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos\' actions and restore balance to the universe.'},
+      {title: 'Black Panther',  id:'blackPanther',synopsis: 'T\'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country\'s past.'}
+    ];
+}
+module.exports = getMovies;
+
+// return [
+//     {title: 'The Matrix', img: matrixMovie, id:'matrix',synopsis: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers'},
+//     {title: 'Full Metal Alchemist', img: fullMetalMovie, id:'fullMetal',synopsis: 'When a failed alchemical ritual leaves brothers Edward and Alphonse Elric with severely damaged bodies, they begin searching for the one thing that can save them; the fabled philosopher\'s stone.'},
+//     {title: 'Bleach', img: bleachMovie, id:'bleach',synopsis: 'High school student Kurosaki Ichigo is unlike any ordinary kid because he can see ghosts. After an accident with a hollow, he get powers. So begins Kurosaki Ichigo\'s training and duty as a Shinigami, Soul Reaper'},
+//     {title: 'Iron Man', img: ironManMovie, id:'ironMan',synopsis: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.'},
+//     {title: 'Avengers Endgame', img:avengersEndgameMovie, id:'avengersEndgame',synopsis: 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos\' actions and restore balance to the universe.'},
+//     {title: 'Black Panther', img: blackPantherMovie, id:'blackPanther',synopsis: 'T\'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country\'s past.'}
+// ];
