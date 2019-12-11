94bd95c4e0c58f9f2e400894abe9e6b0b77590dd
maxgdev <max@apphilia.net>
2019-12-11 00:28:34 +0000

    Changed backend port to 3001 for Task #21

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const getmovies = require('./getmovies');
 const app = express();
-const port = 3002;
+const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/rest/shows', (req, res) => res.send(getmovies()))
