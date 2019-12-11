5b676dcc866b8bae5b177fa1f6e033ae80fb54c0
maxgdev <max@apphilia.net>
2019-12-11 00:20:42 +0000

    Completed - Improve setup of backend server - task #20

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const getmovies = require('./getmovies');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3002;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/rest/shows', (req, res) => res.send(getmovies()))
