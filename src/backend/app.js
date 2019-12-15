a20623d90d0e29986630def4836e1d40f5decf6c
maxgdev <max@apphilia.net>
2019-12-15 10:12:51 +0000

    Completed Task #23-2. Production development server

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const getmovies = require('./getmovies');
 const app = express();
-const port = 3001;
+const port = process.env || 3001;
 
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/rest/shows', (req, res) => res.send(getmovies()))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
